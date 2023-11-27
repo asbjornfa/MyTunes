ca4948038130c291190537b99dc1628b52552cc0
asbjornfa <asbjornfa@gmail.com>
2023-11-27 11:45:51 +0100

    Changes in maincontroller

--- src/GUI/MainController.java
+++ src/GUI/MainController.java
@@ -2,14 +2,49 @@ package GUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class MainController {
     @FXML
-    private Label lbl;
-
+    private ListView LstPlayList;
     @FXML
-    private void clickBtn(ActionEvent actionEvent) {
-        lbl.setText("Its Nice!");
-    }
+    private Button btnBackwardsMusic;
+    @FXML
+    private Button btnPlayMusic;
+    @FXML
+    private ListView lstSongsList;
+    @FXML
+    private Button btnForwardMusic;
+    @FXML
+    private Button btnSearchS;
+    @FXML
+    private TextField txtSearchS;
+    @FXML
+    private Slider slidVolume;
+    @FXML
+    private Button btnNewP;
+    @FXML
+    private Button btnEditP;
+    @FXML
+    private Button btnDeleteP;
+    @FXML
+    private Button BtnUpSP;
+    @FXML
+    private Button btnDownSP;
+    @FXML
+    private Button btnDeleteSP;
+    @FXML
+    private Button btnNewS;
+    @FXML
+    private Button btnEditS;
+    @FXML
+    private Button btnDeleteS;
+    @FXML
+    private Button btnCloseS;
+    @FXML
+    private Label lblSName;
+    @FXML
+    private ListView lstOnSP;
+
+
 }
